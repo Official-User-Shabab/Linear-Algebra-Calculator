69885f2e1f63934ca98fe64001ea0c509c85af95
Official-User-Shabab <officialshabab24@gmail.com>
2025-02-22 10:06:40 +0000

    Update draft_main.py

--- draft_main.py
+++ draft_main.py
@@ -8,3 +8,9 @@ vectorB = [3, 2, 1]
 vectorC = [1, 0, 1]
 
 matrix1 = [vectorA, vectorB, vectorC]   # Issue: It is better to handle each column as a vector, so how to imagine this in the form of an array?
+
+#        COLUMNS
+#  R      0 1 2 
+#  O   0 [1 3 1]  This is matrix1
+#  W   1 |2 2 0|  When referencing to a matrix, it must be as (column, row)
+#  s   2 [3 1 1]  Example: To get element 0: matrix1[2, 1]
